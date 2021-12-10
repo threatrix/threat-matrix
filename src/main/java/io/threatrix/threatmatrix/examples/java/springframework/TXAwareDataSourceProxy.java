5a409d8c52d451b6e5f58e99e24ee91bc18ba98e
John McDonald <john@threatrix.io>
2021-12-10 11:59:14 -0800

    update

--- src/main/java/io/threatrix/threatmatrix/examples/java/springframework/TXAwareDataSourceProxy.java
+++ src/main/java/io/threatrix/threatmatrix/examples/java/springframework/TXAwareDataSourceProxy.java
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-framework
+*    Release: https://github.com/spring-projects/spring-framework/releases/tag/v4.3.6.RELEASE
+*    Source File: TransactionAwareDataSourceProxy.java
+*    
+*    Copyrights:
+*      copyright 2002-2013 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package io.threatrix.threatmatrix.utility.springframework;
 
 import java.lang.reflect.InvocationHandler;
