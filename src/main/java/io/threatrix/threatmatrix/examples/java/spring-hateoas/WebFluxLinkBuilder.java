1e9b3dcf6188077d39d5a84e9e055aca2da04ce2
markrouz <markrouz@yandex.ru>
2022-06-24 15:22:58 +0400

    update

--- src/main/java/io/threatrix/threatmatrix/examples/java/spring-hateoas/WebFluxLinkBuilder.java
+++ src/main/java/io/threatrix/threatmatrix/examples/java/spring-hateoas/WebFluxLinkBuilder.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-boot
+*    Release: https://github.com/spring-projects/spring-boot/releases/tag/v1.5.21.RELEASE
+*    Source File: ConfigurationPropertiesBindingPostProcessor.java
+*    
+*    Copyrights:
+*      copyright 2012-2018 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
